461c3d92e99321e83a8131bc77c281923014b185
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-12 17:23:10 -0400

    Add createCard import

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,13 +1,14 @@
 const chai = require('chai')
 const expect = chai.expect
 
+const { createCard } = require('../src/card')
 const { createDeck } = require('../src/deck')
 
 describe("deck", () => {
   let card1, card2, card3, deck;
   beforeEach(() => {
     card1 = createCard(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    card2 = craeteCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     deck = createDeck([card1, card2, card3])
   });
@@ -21,13 +22,13 @@ describe("deck", () => {
     expect(deck).to.deep.equal([card1, card2, card3])
   });
 
-  
+
   //it block 
     // test the createDeck method is it a function
     // create 3 or 4 cards from createDeck function as placeholders
     // ref the example
     // test the output is an array
-    console.log(createDeck)
+    
 
   //execution
   //create vars for the function
@@ -37,5 +38,5 @@ describe("deck", () => {
   //countCards it block
 })
 
-//write seaparate tests for 3 placeholder object cards to pass thru like vampire, create an array and will work on an arg array
+
 

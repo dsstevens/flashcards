b90563eac89f296b8fa2764c96231c4319ff4b2c
Deanna Stevens <deanna.sofias@gmail.com>
2023-10-12 21:32:18 -0400

    Add chai setup for describe round

---
+++ test/Round-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const { createCard, evaluateGuess } = require('../src/card');
+const { createDeck } = require('../src/deck');
+const { createRound } = require('../src/round');
+
+describe("round", () => {
+  let card1, card2, card3, deck, round;
+
+beforeEach(() => {
+  
+})
+
+
+})
+
+
+
